1a9f7d7da71848c70e5698fb8511d46831f4794e
SaraBalbin <51859641+SaraBalbin@users.noreply.github.com>
2025-04-11 18:35:00 -0500

    Taller 2 - parte 1

--- homework/word_count.py
+++ homework/word_count.py
@@ -5,9 +5,9 @@
 import fileinput
 import glob
 import os.path
+import string
 import time
 from itertools import groupby
-
 
 #
 # Escriba la funcion que  genere n copias de los archivos de texto en la
@@ -17,10 +17,25 @@ from itertools import groupby
 # original se llama text0.txt, el archivo generado se llamará text0_1.txt,
 # text0_2.txt, etc.
 #
+
+
 def copy_raw_files_to_input_folder(n):
     """Funcion copy_files"""
 
+    if not os.path.exists("files/input"):
+        os.makedirs("files/input")
 
+    for file in glob.glob("files/raw/*"):
+        for i in range(1, n + 1):
+            with open(file, "r", encoding="utf-8") as f:
+                with open(
+                    f"files/input/{os.path.basename(file).split('.')[0]}_{i}.txt",
+                    "w",
+                    encoding="utf-8",
+                ) as f2:
+                    f2.write(f.read())
+
+                    
 #
 # Escriba la función load_input que recive como parámetro un folder y retorna
 # una lista de tuplas donde el primer elemento de cada tupla es el nombre del
@@ -39,6 +54,22 @@ def copy_raw_files_to_input_folder(n):
 def load_input(input_directory):
     """Funcion load_input"""
 
+    # Creamos una lista vacía donde se almacenarán las líneas de los archivos junto con sus nombres
+    sequence = []
+
+    # Usamos glob para obtener todos los archivos dentro del directorio de entrada (input_directory)
+    # El patrón '*' indica que queremos todos los archivos sin importar su extensión
+    files = glob.glob(f"{input_directory}/*")
+
+    # Abrimos todos los archivos a la vez usando fileinput, que permite iterar sobre varios archivos como si fueran uno solo
+    with fileinput.input(files=files) as f:
+        # Iteramos línea por línea a través de todos los archivos
+        for line in f:
+            # Agregamos a la lista 'sequence' una tupla que contiene:
+            # - El nombre del archivo actual
+            # - La línea de texto actual
+            sequence.append((fileinput.filename(), line))
+    return sequence
 
 #
 # Escriba la función line_preprocessing que recibe una lista de tuplas de la
@@ -47,8 +78,12 @@ def load_input(input_directory):
 #
 def line_preprocessing(sequence):
     """Line Preprocessing"""
-
-
+    sequence = [
+        (key, value.translate(str.maketrans("", "", string.punctuation)).lower())
+        for key, value in sequence
+    ]
+    return sequence
+  
 #
 # Escriba una función llamada maper que recibe una lista de tuplas de la
 # función anterior y retorna una lista de tuplas (clave, valor). En este caso,
@@ -61,9 +96,10 @@ def line_preprocessing(sequence):
 #     ...
 #   ]
 #
+
 def mapper(sequence):
     """Mapper"""
-
+    return [(word, 1) for _, value in sequence for word in value.split()]
 
 #
 # Escriba la función shuffle_and_sort que recibe la lista de tuplas entregada
@@ -120,9 +156,16 @@ def create_marker(output_directory):
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
-#
+from pprint import pprint
+
+
 def run_job(input_directory, output_directory):
     """Job"""
+
+    sequence = load_input(input_directory)
+    sequence = line_preprocessing(sequence)
+    sequence = mapper(sequence)
+    pprint(sequence[:5])
 
 
 if __name__ == "__main__":
